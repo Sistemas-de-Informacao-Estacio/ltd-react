9170e38ff9fdc490624eceb59cb0df3dc29c717f
Raíssa <raissajoaquim29@gmail.com>
2025-05-06 14:43:02 -0300

    alterações na home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,21 +5,18 @@ import About from "../Components/About";
 function Home() {
   const [app, setApp] = useState('Home');
 
-  const handleApp = (page) => {
+  const handleNavegação = (page) => {
     setApp(page);
   }
 
-  const renderContent = () => {
-    switch (app) {
-      case 'Apps':
-        return <Apps />;
-      case 'Sobre':
-        return <About />;
-    }
-  }
+ if (app === 'Apps') 
+  return <Apps/>;
+ if (app === 'Sobre')
+  return <About/>
 
+ 
   return (
-    <div className="min-h-screen scroll-smooth pt-24 bg-gray-900 text-white">
+    <div className="h-screen  pt-24 bg-gray-900 text-white overflow-hidden">
       <section className="text-center p-8">
 
         <h1 className="text-4xl font-bold bg-gradient-to-r from-blue-300 to-blue-500 bg-clip-text text-transparent mb-6 animate-pulse leading-tight">
@@ -31,21 +28,16 @@ function Home() {
 
         <div className="mt-6 flex justify-center gap-4">
 
-          <button onClick={() => handleApp('Apps')} className="bg-blue-600 text-white px-6 py-2 rounded-full hover:bg-blue-700 hover:shadow-lg transition-all duration-300">
+          <button onClick={() => handleNavegação('Apps')} className="bg-blue-600 text-white px-6 py-2 rounded-full hover:bg-blue-700 hover:shadow-lg transition-all duration-300">
             Nossos Aplicativos
           </button>
 
-          <button onClick={() => handleApp('Sobre')} className="bg-transparent border-2 border-blue-600 text-blue-600 px-6 py-2 rounded hover:bg-blue-600 hover:text-white transition">
+          <button onClick={() => handleNavegação('Sobre')} className="bg-transparent border-2 border-blue-600 text-blue-600 px-6 py-2 rounded hover:bg-blue-600 hover:text-white transition">
             Saiba mais
           </button>
 
         </div >
       </section>
-
-
-
-
-      <div className="mt-50">{renderContent()}</div>
     </div>
 
   )
