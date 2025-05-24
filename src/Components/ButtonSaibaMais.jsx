211fedfa4f4a105c51ecafa956e0657faad9a926
Estevam <contato@estevamsouza.com.br>
2025-05-23 23:02:25 -0300

    feat(components): atualizar componente ButtonSaibaMais

--- src/Components/ButtonSaibaMais.jsx
+++ src/Components/ButtonSaibaMais.jsx
@@ -1,4 +1,12 @@
-function ButtonSaibaMais({onClick}) {
+function ButtonSaibaMais({onClick, to}) {
+    if (to) {
+        return (
+            <span className="text-white bg-blue-500 border-2 border-blue-800 px-20 py-2 rounded-2xl hover:bg-blue-800 hover:text-white transition cursor-pointer inline-block">
+                Saiba mais
+            </span>
+        );
+    }
+    
     return (
         <button onClick={onClick} className="text-white bg-blue-500 border-2 border-blue-800 px-20 py-2 rounded-2xl hover:bg-blue-800 hover:text-white transition">
             Saiba mais
